d53496a084f1d79210379226d4578277bdcc99d3
praful <prafulraut227@gmail.com>
2022-05-03 21:01:14 +0530

    project

---
+++ htmlProject/js/prafulJS/js/prafulJS/1.js
@@ -0,0 +1,13 @@
+
+function borderIn(x) {
+    x.style.border = "2px solid red";
+    // x.style.background = "yellow"
+  }
+  
+  function borderOut(x) {
+    x.style.border = "2px solid black";
+
+  }
+ 
+
+  
